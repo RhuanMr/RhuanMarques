e084ddf9c599bc60ac8eea84310441eca295ed7a
Rhuan Marques <magicknight@hotmail.com.br>
2021-01-31 15:02:02 -0300

    Create the react-native project

--- curriculo/src/components/AboutMe/index.js
+++ curriculo/src/components/AboutMe/index.js
@@ -6,7 +6,7 @@ export default  class AboutMe extends Component {
     return(
       <div className="container-about" id="aboutMe">
            <h1>Um pouco sobre mim:</h1>
-           <p>Me chamo Rhuan Marques, tenho 21 anos e curso o nono semestre de Engenharia de software na Universidade de Bras~ilia (UnB).</p>
+           <p>Me chamo Rhuan Marques, tenho 21 anos e curso o nono semestre de Engenharia de software na Universidade de Brasília (UnB).</p>
       </div>
     )
   }
